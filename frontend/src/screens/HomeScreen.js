8bf5ec17332788f2c25b40004dd398e80e11e258
Cj458 <Calebjason90@gmail.com>
2021-05-12 21:26:36 +0300

    final project

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -37,11 +37,11 @@ export default function HomeScreen() {
       ) : (
         <>
           {sellers.length === 0 && <MessageBox>No Seller Found</MessageBox>}
-          <Carousel showArrows autoPlay showThumbs={false}>
+          <Carousel className="image" showArrows autoPlay showThumbs={false}>
             {sellers.map((seller) => (
               <div key={seller._id}>
                 <Link to={`/seller/${seller._id}`}>
-                  <img src={seller.seller.logo} alt={seller.seller.name} />
+                  <img  src={seller.seller.logo} alt={seller.seller.name} />
                   <p className="legend">{seller.seller.name}</p>
                 </Link>
               </div>
